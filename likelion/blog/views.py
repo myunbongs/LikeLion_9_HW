e02e60fa91332e567222338d6a6e447af37e918a
igoo-Y <yang_ig@naver.com>
2021-05-21 18:21:47 +0900

    crud

--- likelion/blog/views.py
+++ likelion/blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
 # Create your views here.
+
+def home(request):
+    blogs = Blog.objects.all()
+    return render(request, 'home.html', {'blogs': blogs})
+
+def detail(request, blog_id):
+    # blog = Blog.object.get(blog_id = id)
+    blog = get_object_or_404(Blog, pk = blog_id)
+    return render(request, 'detail.html', {'blog':blog})
+
+def new(request):
+    return render(request, 'new.html')
--- likelion/likelion/urls.py
+++ likelion/likelion/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name="home"),
+    path('<str:blog_id>', detail, name="detail")
+    path('new/', new, name="new")
 ]
