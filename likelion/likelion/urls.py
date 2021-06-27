98c94489e94c51ff4ada7b4961fac9ceb04b8f94
Ingyu Yang <79055280+igoo-Y@users.noreply.github.com>
2021-06-27 15:15:29 +0900

    form

--- likelion/likelion/urls.py
+++ likelion/likelion/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from blog.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
-    path('blog/', include('blog.urls'))
-]
+    path('blog/', include('blog.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
